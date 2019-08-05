4c1a05e8fe94d1d800e4667b8734347401a1d5ff
Evan Pipho <evan@10000coins.com>
2019-08-05 11:39:29 -0700

    Hook to service

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
@@ -75,21 +79,22 @@ func main() {
 		}
 	}()
 
-	err = start_scoring(*name, *email, *class)
+	id, err := start_scoring(*url, *key, *name, *email, *class)
+	_ = id
 	if err != nil {
 		fmt.Println("Unable to start scoring:", err)
 		os.Exit(1)
 	}
-	err = score(outer, inner, errchan)
+	err = score(outer, inner, errchan, *url, *key, id)
 	if err != nil {
 		fmt.Println("Unable to complete scoring:", err)
-		cancel_scoring()
+		cancel_scoring(*url, *key, id)
 		os.Exit(1)
 	}
 	fmt.Println("DONE")
 }
 
-func score(outer *tail.Tail, inner *tail.Tail, errchan <-chan error) error {
+func score(outer *tail.Tail, inner *tail.Tail, errchan <-chan error, url string, key string, id string) error {
 	outer_samples := 0
 	inner_samples := 0
 	outer_buckets := []float32{}
@@ -128,7 +133,7 @@ func score(outer *tail.Tail, inner *tail.Tail, errchan <-chan error) error {
 				inner_buckets = process_sample(ln.Text, inner_buckets)
 			}
 		case <-ticker.C:
-			update_score(outer_buckets, outer_samples, inner_buckets, inner_samples, false)
+			update_score(outer_buckets, outer_samples, inner_buckets, inner_samples, url, key, id, false)
 		case err := <-errchan:
 			return err
 		}
@@ -136,8 +141,7 @@ func score(outer *tail.Tail, inner *tail.Tail, errchan <-chan error) error {
 			break
 		}
 	}
-	update_score(outer_buckets, outer_samples, inner_buckets, inner_samples, true)
-	return nil
+	return update_score(outer_buckets, outer_samples, inner_buckets, inner_samples, url, key, id, true)
 }
 
 func process_sample(line string, buckets []float32) []float32 {
@@ -160,19 +164,43 @@ func process_sample(line string, buckets []float32) []float32 {
 	return buckets
 }
 
-func start_scoring(name string, email string, class string) error {
-	return nil
+func start_scoring(url string, key string, name string, email string, class string) (string, error) {
+	type CreateScoreRequest struct {
+		Name  string  `json:"name"`
+		Class string  `json:"class"`
+		Email *string `json:"email",omitempty`
+	}
+	type CreateScoreResponse struct {
+		ID string `json:"id"`
+	}
+
+	req := &CreateScoreRequest{
+		Name:  name,
+		Class: class,
+	}
+	if len(email) != 0 {
+		req.Email = &email
+	}
+	resp := &CreateScoreResponse{}
+
+	err := do_request(url+"/admin/score", key, "POST", req, resp)
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Printf("Scoring started for %s in %s\n", name, class)
+	fmt.Printf("Score ID: %s\n", resp.ID)
+	return resp.ID, nil
 }
 
-func cancel_scoring() error {
-	return nil
+func cancel_scoring(url string, key string, id string) error {
+	fmt.Printf("Canceling scoring for %s\n", id)
+	return do_request(url+"/admin/score/"+id, key, "DELETE", nil, nil)
 }
 
-func update_score(outer_buckets []float32, outer_samples int, inner_buckets []float32, inner_samples int, commit bool) {
+func update_score(outer_buckets []float32, outer_samples int, inner_buckets []float32, inner_samples int, url string, key string, id string, commit bool) error {
 	max := float32(-100.0)
 	min := float32(100.0)
-
-	fmt.Println(outer_buckets[0], inner_buckets[0])
 
 	for i := range outer_buckets {
 		if i < len(inner_buckets) {
@@ -185,5 +213,49 @@ func update_score(outer_buckets []float32, outer_samples int, inner_buckets []fl
 			}
 		}
 	}
-	fmt.Println(min, max)
+
+	type UpdateScoreRequest struct {
+		Score    float32 `json:"score"`
+		Finalize bool    `json:"finalize"`
+	}
+	upd := &UpdateScoreRequest{
+		Score:    min + max,
+		Finalize: commit,
+	}
+	fmt.Printf("Current Score: %f\n", upd.Score)
+	return do_request(url+"/admin/score/"+id, key, "POST", upd, nil)
+}
+
+func do_request(url string, key string, method string, in interface{}, out interface{}) error {
+	body, err := json.Marshal(in)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+key)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	resp_body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode < 200 || resp.StatusCode >= 400 {
+		return fmt.Errorf("Communication error: %s (%s)", resp.Status, string(resp_body))
+	}
+
+	if out != nil {
+		return json.Unmarshal(resp_body, out)
+	}
+	return nil
 }
--- test.sh
+++ test.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
+set -eu
+
 go build -o bin/tfh_scorer
 
 touch sweep_ta.txt
 touch sweep_tb.txt
 
-./bin/tfh_scorer -u a -k a sweep_ta.txt sweep_tb.txt &
+./bin/tfh_scorer -u http://localhost:8080 -k abc -n new -c unlimited sweep_ta.txt sweep_tb.txt &
 
-cat sweep_a.txt | pv -q -L 1000000 > sweep_ta.txt &
-cat sweep_b.txt | pv -q -L 1000000 > sweep_tb.txt
+cat sweep_a.txt | pv -q -L 10000000 > sweep_ta.txt &
+cat sweep_b.txt | pv -q -L 10000000 > sweep_tb.txt
 
 kill -USR1 $(ps -a | grep tfh_scorer | awk '{print $1}')
